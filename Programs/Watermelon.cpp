0b4c724347babc5092bf3b0d1886ab84a6b6916b
vlatkosh <vsarkovskii@gmail.com>
2018-04-09 18:35:12 +0200

    Add files via upload

---
+++ Programs/Watermelon.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+//    std::ios::sync_with_stdio(false);
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+    if (n % 2 == 0 && n > 2) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
