65d3595618adb7c412095a5668106d6b0f246849
vlatkosh <vsarkovskii@gmail.com>
2018-04-10 19:08:43 +0200

    added DP solution

--- Programs/Prodavnica.cpp
+++ Programs/Prodavnica.cpp
@@ -1,6 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
+int main()
+{
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+
+    //Dynamic programming algorithm (for exercise, I am learning it RN (2018/04/10)))
+    int P, V;
+    cin >> P >> V;
+
+    int n = V - P;
+    vector<int> coins = {1, 2, 5};
+    sort(coins.begin(), coins.end());
+
+    int dp[n+1];
+    memset(dp, 100000000, sizeof(dp));
+    dp[0] = 0;
+
+    for (int amount = 1; amount <= n; ++amount)
+    {
+        for (int coinValue : coins)
+        {
+            if (coinValue > amount) break;
+            int from = amount - coinValue; //najefikasnoto od prethodna presmetka
+            dp[amount] = min(dp[amount], dp[from]+1);
+        }
+    }
+
+    cout << dp[n];
+}
+
+
+/* EZ solution (Greedy)
+#include <bits/stdc++.h>
+using namespace std;
+
 int main() {
     int p, v; cin >> p >> v;
     int k = v - p;
@@ -16,3 +51,4 @@ int main() {
     }
     cout << c << endl;
 }
+*/
