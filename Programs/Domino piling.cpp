87cb4a7204f773f9ccb27ca69900c7f2d550d79b
vlatkosh <vsarkovskii@gmail.com>
2018-04-24 18:38:22 +0200

    Create Domino piling.cpp

---
+++ Programs/Domino piling.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+
+    int m, n;
+    cin >> m >> n;
+    cout << m*n/2;
+}
