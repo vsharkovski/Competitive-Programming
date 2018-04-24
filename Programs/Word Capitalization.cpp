a9d93565b20ed8d92676b24345249d7b63cfcc17
vlatkosh <vsarkovskii@gmail.com>
2018-04-24 19:40:04 +0200

    Create Word Capitalization.cpp

---
+++ Programs/Word Capitalization.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+
+    string s;
+    cin >> s;
+    cout << (char)toupper(s[0]);
+    cout << s.substr(1);
+}
