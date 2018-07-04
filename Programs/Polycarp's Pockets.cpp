6ff5f1176ed95f72093b2f5dd89ce709554f7e27
vlatkosh <vsarkovskii@gmail.com>
2018-07-04 11:22:04 +0200

    Create Polycarp's Pockets.cpp

---
+++ Programs/Polycarp's Pockets.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <complex>
+#include <utility>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <bitset>
+
+#define endl '\n'
+#define EPS 1e-9
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> pii;
+
+
+void SOLVE() {
+
+  int n, ans = -1;
+  cin >> n;
+  map<int, int> m;
+  for (int i = 0; i < n; ++i) {
+    int x;
+    cin >> x;
+    ++m[x];
+    ans = max(ans, m[x]);
+  }
+  cout << ans << endl;
+
+}
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  SOLVE();
+}
+
+
+
+
+
+
+
+
+
+
+
+
