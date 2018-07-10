7044e78e84999f526138ed9da5269c309d71d7ca
vlatkosh <vsarkovskii@gmail.com>
2018-07-10 11:34:19 +0200

    Create Sum of Different Primes.cpp

---
+++ Programs/Sum of Different Primes.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <complex>
+#include <utility>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <bitset>
+
+#define endl '\n'
+#define EPS 1e-9
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> pii;
+
+
+vector<int> primes;
+int numprimes;
+
+int dp[200][1125][15];
+
+int f(int pid, int remN, int remK) {
+  if (pid == numprimes || remN == 0 || remK == 0) {
+    return (remN == 0 && remK == 0);
+  }
+  
+  int& ref = dp[pid][remN][remK];
+  if (ref != -1) return ref;
+  
+  if (primes[pid] > remN) {
+    return ref = 0 + f(pid+1, remN, remK);  
+  } else {
+    return ref = 0 + f(pid+1, remN, remK) + f(pid+1, remN-primes[pid], remK-1);
+  }
+
+}
+
+
+void SOLVE() {
+  primes = {
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+    173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367,
+    373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577,
+    587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809,
+    811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021,
+    1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117
+  };
+  numprimes = primes.size();
+  
+  int n, k;
+  while (cin >> n >> k, n != 0 && k != 0) {
+    memset(dp, -1, sizeof(dp));
+    cout << f(0, n, k) << endl;
+  }
+  
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  SOLVE();
+}
