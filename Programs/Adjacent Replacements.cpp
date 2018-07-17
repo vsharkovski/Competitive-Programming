d0be27b54646f4864be3e2a2783b3b862c87f3fd
vlatkosh <vsarkovskii@gmail.com>
2018-07-17 11:46:12 +0200

    Create Adjacent Replacements.cpp

---
+++ Programs/Adjacent Replacements.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <complex>
+#include <utility>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <bitset>
+#include <sstream>
+
+#define endl '\n'
+#define EPS 1e-9
+#define all(v) begin(v), end(v)
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+const int G_V = 1, G_U = -1; //graph visited constant, graph unvisited constant
+
+
+
+
+
+
+void SOLVE() {
+
+  int n, x;
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    cin >> x;
+    if (x % 2 == 0) --x;
+    cout << x << ' ';
+  }
+
+
+
+
+
+
+}
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  SOLVE();
+}
+
+
+
+
+
+
