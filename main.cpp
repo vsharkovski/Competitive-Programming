ac22983b482692892ce4a33d0ffc54e0c536e4a9
vlatkosh <vsarkovskii@gmail.com>
2018-07-21 12:47:19 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <complex>
+#include <utility>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <bitset>
+#include <sstream>
+
+#define endl '\n'
+#define EPS 1e-9
+#define all(v) begin(v), end(v)
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+
+
+
+
+
+
+
+
+
+
+
+
+void SOLVE() {
+  
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  SOLVE();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
