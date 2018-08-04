2ca1e458d1f9b3e4de08cae325566e3c569ac64d
vlatkosh <vsarkovskii@gmail.com>
2018-08-04 19:08:42 +0200

    Create 11069 - A Graph Problem.cpp

---
+++ Programs/UVa/11069 - A Graph Problem.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+#include <bitset>
+#include <sstream>
+
+#define endl '\n'
+#define EPS 1e-9
+#define all(v) begin(v), end(v)
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+
+
+void MAIN() {
+
+  int dp[80];
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 2;
+  for (int x = 4; x <= 76; ++x) {
+    dp[x] = dp[x-3] + dp[x-2];
+  }
+
+  int n;
+  while (cin >> n) {
+    cout << dp[n] << endl;
+  }
+
+}
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  MAIN();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
