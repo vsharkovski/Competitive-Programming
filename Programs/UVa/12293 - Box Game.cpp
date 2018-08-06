7c4e18bec366a0854789abac044666830268b6c4
vlatkosh <vsarkovskii@gmail.com>
2018-08-06 10:27:34 +0200

    Create 12293 - Box Game.cpp

---
+++ Programs/UVa/12293 - Box Game.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+#include <bitset>
+#include <sstream>
+
+#define endl '\n'
+#define EPS 1e-9
+#define all(v) begin(v), end(v)
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+
+
+
+
+
+
+void MAIN() {
+
+  // Bob always win if n is 1 less than a power of 2
+  int n;
+  while (cin >> n, n != 0) {
+    cout << ((n+1)&(n+1-1) ? "Alice\n" : "Bob\n");
+  }
+
+
+
+}
+
+
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  MAIN();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
