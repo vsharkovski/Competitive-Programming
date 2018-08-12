591c81efaf201e02db057be7538ec253b3ef319e
vlatkosh <vsarkovskii@gmail.com>
2018-08-12 11:34:11 +0200

    Create 888B - Buggy Robot.cpp

---
+++ Programs/Codeforces/888B - Buggy Robot.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define EPS 1e-9
+#define all(v) begin(v), end(v)
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+
+const int mod = 1e9 + 7;
+template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){ if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+
+
+
+
+void Solve() {
+  int n, cnt[200] = {};
+  string s;
+  cin >> n >> s;
+  for (char ch : s) cnt[(int)ch]++;
+  cout << 2*min(cnt['U'], cnt['D']) + 2*min(cnt['L'], cnt['R']) << endl;
+}
+
+
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
+
+
+
+
+
+
+
+
