3aa0880a13710214cc953b7ee8a2d0d104fcbd88
vlatkosh <vsarkovskii@gmail.com>
2018-08-12 18:22:44 +0200

    Update N choose K (Binomial Coefficients) with Dynamic Programming.cpp

--- Other/N choose K (Binomial Coefficients) with Dynamic Programming.cpp
+++ Other/N choose K (Binomial Coefficients) with Dynamic Programming.cpp
@@ -1,3 +1,5 @@
+const long long mod = 998244353;
+
 long long C(int n, int k) {
   static const int cMAXN = 60;
   static long long cdp[cMAXN][cMAXN];
@@ -8,5 +10,5 @@ long long C(int n, int k) {
   if (n < k) return 0;
   if (k == 0 || n == k) return 1;
   if (cdp[n][k] != -1) return cdp[n][k];
-  return cdp[n][k] = C(n-1, k-1) + C(n-1, k);
+  return cdp[n][k] = (C(n-1, k-1) + C(n-1, k)) % mod;
 }
