d86ab439973b532c4e2d6f778c3789141f70d26b
vlatkosh <vsarkovskii@gmail.com>
2018-09-01 09:03:18 +0200

    Create 1028B - Unnatural Conditions.cpp

---
+++ Programs/Codeforces/1028B - Unnatural Conditions.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define EPS 1e-9
+#define all(v) begin(v), end(v)
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+
+const ll mod = 1e9 + 7;
+template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){ if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+
+
+
+
+
+
+void Solve() {
+
+  string a = "", b = "";
+  
+  for (int i = 0; i < 1128; ++i) {
+    a += '4';
+  }
+  a += '5';
+  for (int i = 0; i < 1129; ++i) {
+    b += '5';
+  }
+  cout << a << endl << b << endl;
+
+
+
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
+
+
+
+
+
+
+
