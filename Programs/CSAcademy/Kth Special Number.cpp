f1c27093973f2b469627da977f08385772182d73
vlatkosh <vsarkovskii@gmail.com>
2018-09-09 09:39:46 +0200

    Create Kth Special Number.cpp

---
+++ Programs/CSAcademy/Kth Special Number.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define EPS 1e-9
+#define all(v) begin(v), end(v)
+
+using namespace std;
+typedef long long ll;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+
+const ll mod = 1e9 + 7;
+template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){ if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+
+
+
+
+
+int msbpos = 1;
+
+bool ima(int x) {
+  for (int i = 0; i < msbpos-1; ++i) {
+    if ((x & (1 << i)) && (x & (1 << (i+1)))) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void Solve() {
+
+  int k;
+  cin >> k;
+  --k;
+  
+  int x = 1;
+  while (k--) {
+//    cout << x << " " << bitset<4>(x) << endl;
+    do {
+      ++x;
+      while (x >= (1 << msbpos)) ++msbpos;
+    } while (ima(x));
+  }
+  cout << x << endl;
+
+
+
+
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
