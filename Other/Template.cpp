330a13399f3244809cd81f1fe9dec8e3cd28b889
vlatkosh <vsarkovskii@gmail.com>
2018-09-15 09:10:44 +0200

    Update Template.cpp

--- Other/Template.cpp
+++ Other/Template.cpp
@@ -6,27 +6,31 @@
 
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 typedef std::pair<int, int> ii;
 typedef std::vector<ii> vii;
 typedef std::vector<int> vi;
 
-const int mod = 1e9 + 7;
+const ll mod = 1e9 + 7;
 template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
 template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){ if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
 
 
 
 
+
+
+
 void Solve() {
-
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
+  
 }
-
-
-
 
 int main() {
   std::ios::sync_with_stdio(false);
@@ -43,5 +47,5 @@ int main() {
 
 
 
-
-
+                        
+                        
