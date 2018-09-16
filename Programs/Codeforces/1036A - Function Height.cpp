e11d6e379f93b9727a68adf68e4db39d63cd8baf
vlatkosh <vsarkovskii@gmail.com>
2018-09-16 09:14:03 +0200

    Create 1036A - Function Height.cpp

---
+++ Programs/Codeforces/1036A - Function Height.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+ 
+#define endl '\n'
+#define EPS 1e-9
+#define all(v) begin(v), end(v)
+ 
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+ 
+ 
+const ll mod = 1e9 + 7;
+template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){ if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+ 
+
+
+
+
+
+
+void Solve() {
+
+  ll n, k;
+  cin >> n >> k;
+  
+  ll ans = k/n + !!(k%n);
+  cout << ans << endl;
+
+
+
+
+}
+ 
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
+
+
