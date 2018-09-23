bb8e8a8f879868e715960e73546a8f894ae9934d
vlatkosh <vsarkovskii@gmail.com>
2018-09-23 10:14:54 +0200

    Create 1000D - Yet Another Problem On a Subsequence.cpp

---
+++ Programs/Codeforces/1000D - Yet Another Problem On a Subsequence.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iterator>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <list>
+using namespace std;
+// printing
+#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); _dbg(_it, args); }
+void _dbg(istream_iterator<string> it){++it;}
+template<typename T, typename... Args>
+void _dbg(istream_iterator<string> it, T a, Args... args){
+  if (*it=="'\\n'" || *it=="\"\\n\"" || *it=="endl") { cout << "\n"; }
+  else { cout << setw(8) << std::left << *it << " = " << setw(4) << std::right << a << "\n"; }
+  _dbg(++it, args...); }
+template<typename T>
+std::ostream& _containerprint(std::ostream &out, T const &val) { return (out << setw(4) << val << " "); }
+template<typename T1, typename T2>
+std::ostream& _containerprint(std::ostream &out, std::pair<T1, T2> const &val) { return (out << "{" << setw(4) << val.first << " " << setw(4) << val.second << "} "); }
+template<template<typename, typename...> class TT, typename... Args>
+std::ostream& operator<<(std::ostream &out, const TT<Args...> &cont) { for(auto&& elem : cont) { _containerprint(out, elem); } return out; }
+std::ostream& operator<<(std::ostream &out, const std::string &s) { return operator << <char>(out, s); }
+// other
+#define endl '\n'
+#define all(v) begin(v), end(v)
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+const double EPS = 1e-9;
+const ll mod = 998244353;
+template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+
+
+
+
+
+
+
+
+
+void Solve() {
+
+  int a[1001];
+  int dp[1001];
+  int C[1001][1001];
+  
+  for (int n = 0; n <= 1000; ++n) {
+    C[n][0] = C[n][n] = 1;
+    for (int k = 1; k < n; ++k) {
+      C[n][k] = (C[n-1][k] + C[n-1][k-1]) % mod;
+    }
+  }
+
+  int n;
+  cin >> n;
+  
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  
+  int ans = 0;
+  
+  dp[n] = 1;
+  for (int i = n-1; i >= 0; --i) {
+    if (a[i] <= 0) {
+      dp[i] = 0;
+      continue;
+    }
+    dp[i] = 0;
+    for (int j = i+a[i]+1; j <= n; ++j) {
+      dp[i] += ((ll)dp[j] * (ll)C[j-i-1][a[i]]) % mod;
+      dp[i] %= mod;
+    }
+    ans += dp[i];
+    ans %= mod;
+  }
+  
+  cout << ans << endl;
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
