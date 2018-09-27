848adf15191b232dabe68dedd5bcde6e4695568f
vlatkosh <vsarkovskii@gmail.com>
2018-09-27 18:32:55 +0200

    Create 988D - Points and Powers of Two.cpp

---
+++ Programs/Codeforces/988D - Points and Powers of Two.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iterator>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <list>
+#include <stack>
+using namespace std;
+// printing
+#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); _dbg(_it, args); }
+void _dbg(istream_iterator<string> it){++it;}
+template<typename T, typename... Args>
+void _dbg(istream_iterator<string> it, T a, Args... args){
+  if (*it=="'\\n'" || *it=="\"\\n\"" || *it=="endl") { cout << "\n"; }
+  else { cout << setw(8) << std::left << *it << " = " << setw(4) << std::right << a << "\n"; }
+  _dbg(++it, args...); }
+template<typename T>
+std::ostream& _containerprint(std::ostream &out, T const &val) { return (out << setw(4) << val << " "); }
+template<typename T1, typename T2>
+std::ostream& _containerprint(std::ostream &out, std::pair<T1, T2> const &val) { return (out << "{" << setw(4) << val.first << " " << setw(4) << val.second << "} "); }
+template<template<typename, typename...> class TT, typename... Args>
+std::ostream& operator<<(std::ostream &out, const TT<Args...> &cont) { for(auto&& elem : cont) { _containerprint(out, elem); } return out; }
+std::ostream& operator<<(std::ostream &out, const std::string &s) { return operator << <char>(out, s); }
+// other
+#define endl '\n'
+#define all(v) begin(v), end(v)
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+const double EPS = 1e-9;
+const ll mod = 998244353;
+template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Solve() {
+  
+  int n;
+  cin >> n;
+  
+  set<ll> s;
+  for (int i = 0; i < n; ++i) {
+    ll x;
+    cin >> x;
+    s.insert(x);
+  }
+  
+  ll inf = 1e15;
+  ll dx = -inf, dy = -inf;
+  
+  for (ll y : s) {
+    for (ll d = 1; d < (1LL<<31); d <<= 1) {
+      ll x = y-d, z = y+d;
+      if (s.count(x)) {
+        if (dx == -inf) {
+          dx = x;
+          dy = y;
+        }
+        if (s.count(z)) {
+          cout << "3\n" << x << " " << y << " " << z << endl;
+          return;
+        }
+      }
+    }
+  }
+  
+  if (dx != -inf) {
+    cout << "2\n" << dx << " " << dy << endl;
+  } else {
+    cout << "1\n" << *s.begin() << endl;
+  }
+  
+  
+  
+
+
+
+}
+
+
+
+
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
