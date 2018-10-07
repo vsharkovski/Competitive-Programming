c4b5606cbbeb6c021481845a9c992e89db17e8b8
vlatkosh <vsarkovskii@gmail.com>
2018-10-07 14:53:43 +0200

    Create Komisija (2009).cpp

---
+++ Programs/Mendo/Komisija (2009).cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iterator>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <list>
+#include <stack>
+using namespace std;
+// printing
+#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); _dbg(_it, args); }
+void _dbg(istream_iterator<string> it){++it;}
+template<typename T, typename... Args>
+void _dbg(istream_iterator<string> it, T a, Args... args){
+  if (*it=="'\\n'" || *it=="\"\\n\"" || *it=="endl") { cout << "\n"; }
+  else { cout << setw(8) << std::left << *it << " = " << setw(4) << std::right << a << "\n"; }
+  _dbg(++it, args...); }
+template<typename T>
+std::ostream& _containerprint(std::ostream &out, T const &val) { return (out << setw(4) << val << " "); }
+template<typename T1, typename T2>
+std::ostream& _containerprint(std::ostream &out, std::pair<T1, T2> const &val) { return (out << "{" << setw(4) << val.first << " " << setw(4) << val.second << "} "); }
+template<template<typename, typename...> class TT, typename... Args>
+std::ostream& operator<<(std::ostream &out, const TT<Args...> &cont) { for(auto&& elem : cont) { _containerprint(out, elem); } return out; }
+std::ostream& operator<<(std::ostream &out, const std::string &s) { return operator << <char>(out, s); }
+// other
+#define endl '\n'
+#define all(v) begin(v), end(v)
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+const double EPS = 1e-9;
+const int mod = 1000000007;
+template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// br ciklusi, br lugje
+int maxi, maxj;
+int ciklus[20];
+
+int dp[20][20][15010];
+
+// zadaca, clen, br zadaci za ovoj clen
+int f(int i, int j, int k) {
+//  for (int l = 0; l < i; ++l){cout << '|';} cout << "i=" << i << " j=" << j << " k=" << k << endl;
+  
+  if (i == maxi) {
+    if (j+1 == maxj) {
+//      for (int l = 0; l < i; ++l){cout << '|';}
+//      cout << "-ok end " << k << endl;
+      return k;
+    } else {
+//      for (int l = 0; l < i; ++l){cout << '|';}
+//      cout << "-bad end " << 1e8 << endl;
+      return 1e8;
+    }
+  }
+  if (j == maxj) {
+//    for (int l = 0; l < i; ++l){cout << '|';}
+//    cout << "-bad end " << 1e8 << endl;
+    return 1e8;
+  }
+  if (dp[i][j][k] != -1) {
+//    for (int l = 0; l < i; ++l){cout << '|';}
+//    cout << "-already done " << dp[i][j][k] << endl;
+    return dp[i][j][k];
+  }
+  
+  
+  int res = f(i+1, j, k + ciklus[i]);
+  if (k > 0) res = min(res, max(k, f(i+1, j+1, ciklus[i])));
+  
+//  for (int l = 0; l < i; ++l){cout << '|';}
+//  cout << "-from search " << res << endl;
+  
+  return dp[i][j][k] = res;
+}
+
+void Solve() {
+  cin >> maxj >> maxi;
+  for (int i = 0; i < maxi; ++i) {
+    cin >> ciklus[i];
+  }
+  memset(dp, -1, sizeof(dp));
+  cout << f(0, 0, 0) << endl;
+  
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
+    
+  
+}
+  
+  
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
