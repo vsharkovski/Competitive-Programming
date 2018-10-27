0e054a695943ffcc797d837da9c7574239d8595e
vlatkosh <vsarkovskii@gmail.com>
2018-10-27 17:47:36 +0200

    Create 11464 - Even Parity.cpp

---
+++ Programs/UVa/11464 - Even Parity.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iterator>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <list>
+#include <stack>
+using namespace std;
+// printing
+#ifdef _DEBUG
+#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); _dbg(_it, args); }
+void _dbg(istream_iterator<string> it){++it;}
+template<typename T, typename... Args>
+void _dbg(istream_iterator<string> it, T a, Args... args){
+  if (*it=="'\\n'" || *it=="\"\\n\"" || *it=="endl") { cout << "\n"; }
+  else { cout << setw(8) << std::left << *it << " = " << setw(4) << std::right << a << "\n"; }
+  _dbg(++it, args...); }
+template<typename T>
+std::ostream& _containerprint(std::ostream &out, T const &val) { return (out << setw(4) << val << " "); }
+template<typename T1, typename T2>
+std::ostream& _containerprint(std::ostream &out, std::pair<T1, T2> const &val) { return (out << "{" << setw(4) << val.first << " " << setw(4) << val.second << "} "); }
+template<template<typename, typename...> class TT, typename... Args>
+std::ostream& operator<<(std::ostream &out, const TT<Args...> &cont) { for(auto&& elem : cont) { _containerprint(out, elem); } return out; }
+std::ostream& operator<<(std::ostream &out, const std::string &s) { return operator << <char>(out, s); }
+#endif
+// other
+#define endl '\n'
+#define all(v) begin(v), end(v)
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+const double EPS = 1e-9;
+const int mod = 998244353;
+template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+
+
+
+
+
+
+// passes every test case on uDebug but gets WA on the site
+// if you can prove it's incorrect feel free to tell me
+
+int n;
+bool grid[16][16];
+int best;
+
+inline int odd(int r, int c) {
+  return ((r > 0 && grid[r-1][c]) + (r < n && grid[r+1][c]) + (c > 0 && grid[r][c-1]) + (c < n && grid[r][c+1])) % 2;
+}
+
+void dfs(int r, int c, int steps) {
+  if (steps > best) return;
+  if (r == n) {
+    for (c = 0; c < n; ++c)
+      if (odd(r-1, c))
+        return;
+    best = min(best, steps);
+  } else if (c == n) {
+    dfs(r+1, 0, steps);
+  } else if (r == 0) {
+    dfs(r, c+1, steps);
+    if (grid[r][c] == 0) {
+      grid[r][c] = 1;
+      dfs(r, c+1, steps+1);
+      grid[r][c] = 0;
+    }
+  } else {
+    if (odd(r-1, c)) {
+      if (grid[r][c] == 0) {
+        grid[r][c] = 1;
+        dfs(r, c+1, steps+1);
+        grid[r][c] = 0;
+      }
+    } else {
+      dfs(r, c+1, steps);
+    }
+  }
+}
+
+void Solve() {
+  int T;
+  cin >> T;
+  for (int CT = 1; CT <= T; ++CT) {
+    cin >> n;
+    for (int r = 0; r < n; ++r) {
+      for (int c = 0; c < n; ++c) {
+        cin >> grid[r][c];
+      }
+    }
+    best = 1e9;
+    dfs(0, 0, 0);
+    if (best == 1e9) best = -1;
+    cout << "Case " << CT << ": " << best << endl;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+//  std::freopen("in.txt", "r", stdin);
+//  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
