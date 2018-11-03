1d24c9f478464f5d1336ac48b28597646c3bd544
vlatkosh <vsarkovskii@gmail.com>
2018-11-03 22:17:32 +0100

    Create Olimpiski igri.cpp

---
+++ Programs/Mendo/Olimpiski igri.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iterator>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <list>
+#include <stack>
+using namespace std;
+// printing
+#ifdef _DEBUG
+#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); _dbg(_it, args); }
+void _dbg(istream_iterator<string> it){++it;}
+template<typename T, typename... Args>
+void _dbg(istream_iterator<string> it, T a, Args... args){
+  if (*it=="'\\n'" || *it=="\"\\n\"" || *it=="endl") { cout << "\n"; }
+  else { cout << setw(8) << std::left << *it << " = " << setw(4) << std::right << a << "\n"; }
+  _dbg(++it, args...); }
+template<typename T>
+std::ostream& _containerprint(std::ostream &out, T const &val) { return (out << setw(4) << val << " "); }
+template<typename T1, typename T2>
+std::ostream& _containerprint(std::ostream &out, std::pair<T1, T2> const &val) { return (out << "{" << setw(4) << val.first << " " << setw(4) << val.second << "} "); }
+template<template<typename, typename...> class TT, typename... Args>
+std::ostream& operator<<(std::ostream &out, const TT<Args...> &cont) { for(auto&& elem : cont) { _containerprint(out, elem); } return out; }
+std::ostream& operator<<(std::ostream &out, const std::string &s) { return operator << <char>(out, s); }
+#endif
+// other
+#define endl '\n'
+#define all(v) begin(v), end(v)
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+const double EPS = 1e-9;
+const ll mod = 1e9 + 7;
+//template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
+template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+
+
+
+
+
+
+int N;
+int A[100005];
+int dp[100005][4];
+
+int F(int i, int p) {
+  if (i == N) return 0;
+  if (dp[i][p] != -1) return dp[i][p];
+  int res = F(i+1, 0);
+  if ((A[i] == 1 && p != 1) || (A[i] == 2 && p != 2)) {
+    res = max(res, 1 + F(i+1, A[i]));
+  } else if (A[i] == 3) {
+    if (p == 0 || p == 1) {
+      res = max(res, 1 + F(i+1, 2));
+    }
+    if (p == 0 || p == 2) {
+      res = max(res, 1 + F(i+1, 1));
+    }
+  }
+  return dp[i][p] = res;
+}
+
+void Solve() {
+  cin >> N;
+  for (int i = 0; i < N; ++i) {
+    cin >> A[i];
+  }
+  memset(dp, -1, sizeof(dp));
+  cout << F(0, 0) << endl;
+
+
+
+
+
+
+
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
