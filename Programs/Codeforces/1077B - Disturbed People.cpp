e662c94bd2b57d21e3cffee123c5bd0d5f59b747
vlatkosh <vsarkovskii@gmail.com>
2018-11-18 11:21:41 +0100

    Create 1077B - Disturbed People.cpp

---
+++ Programs/Codeforces/1077B - Disturbed People.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define all(v) begin(v), end(v)
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+
+
+
+
+
+
+
+void Solve() {
+  int n;
+  cin >> n;
+  vi a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  int ans = 0;
+  for (int i = 1; i < n-1; ++i) {
+    if (a[i-1] == 1 && a[i] == 0 && a[i+1] == 1) {
+      a[i+1] = 0;
+      ++ans;
+    }
+  }
+  cout << ans << endl;
+
+
+
+
+}
+
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  #ifdef _DEBUG
+  std::freopen("in.txt", "r", stdin);
+  std::freopen("out.txt", "w", stdout);
+  #endif
+  Solve();
+}
