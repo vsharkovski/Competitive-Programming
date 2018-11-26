d4be559fad817d4d29e6af23590590ca87983c84
vlatkosh <vsarkovskii@gmail.com>
2018-11-26 18:03:32 +0100

    Update Template.cpp

--- Other/Template.cpp
+++ Other/Template.cpp
@@ -1,87 +1,36 @@
-#include <iostream>
-#include <iomanip>
-#include <algorithm>
-#include <utility>
-#include <cmath>
-#include <cstring>
-#include <functional>
-#include <iterator>
-#include <bitset>
-#include <vector>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <string>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <list>
-#include <stack>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-// printing
-#ifdef _DEBUG
-#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); _dbg(_it, args); }
-void _dbg(istream_iterator<string> it){++it;}
-template<typename T, typename... Args>
-void _dbg(istream_iterator<string> it, T a, Args... args){
-  if (*it=="'\\n'" || *it=="\"\\n\"" || *it=="endl") { cout << "\n"; }
-  else { cout << setw(8) << std::left << *it << " = " << setw(4) << std::right << a << "\n"; }
-  _dbg(++it, args...); }
-template<typename T>
-std::ostream& _containerprint(std::ostream &out, T const &val) { return (out << setw(4) << val << " "); }
-template<typename T1, typename T2>
-std::ostream& _containerprint(std::ostream &out, std::pair<T1, T2> const &val) { return (out << "{" << setw(4) << val.first << " " << setw(4) << val.second << "} "); }
-template<template<typename, typename...> class TT, typename... Args>
-std::ostream& operator<<(std::ostream &out, const TT<Args...> &cont) { for(auto&& elem : cont) { _containerprint(out, elem); } return out; }
-std::ostream& operator<<(std::ostream &out, const std::string &s) { return operator << <char>(out, s); }
-#endif
-// other
-#define endl '\n'
-#define all(v) begin(v), end(v)
+using namespace __gnu_pbds;
 typedef long long ll;
 typedef long double ld;
-typedef std::pair<int, int> ii;
-typedef std::vector<ii> vii;
-typedef std::vector<int> vi;
-const double EPS = 1e-9;
-const ll mod = 1e9 + 7;
-template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
-template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
 
 
 
-
-
-
-
-
-void Solve() {
-
-
-
-
-
-
-
-
-
-
-
-
-
+void _SOLVE_() {
 
 
 }
 
 
+
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   #ifdef _DEBUG
-  std::freopen("in.txt", "r", stdin);
-  std::freopen("out.txt", "w", stdout);
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
   #endif
-  Solve();
+  _SOLVE_();
 }
