504f4bf844c12efde254b06ff853e5694a73931d
vlatkosh <vsarkovskii@gmail.com>
2018-12-01 13:31:10 +0100

    Create Template-CodeFu.cpp

---
+++ Other/Template-CodeFu.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <cstring>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <list>
+#include <stack>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+
+class a {
+public:
+
+
+
+};
+
+#ifdef _DEBUG
+int main() {
+
+}
+#endif
