82017669c3b2e2226a85128dffc24db02de7d44c
vlatkosh <vsarkovskii@gmail.com>
2018-12-06 17:57:25 +0100

    Create 1088A - Ehab and another construction problem.cpp

---
+++ Programs/Codeforces/1088A - Ehab and another construction problem.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class Key, class Compare = less<Key>> // find_by_order, order_of_key
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+
+
+
+void _SOLVE_() {
+  int x;
+  cin >> x;
+  for (int a = 1; a <= x; ++a) {
+    for (int b = 1; b <= x; ++b) {
+      if (a % b == 0 && a*b > x && a/b < x) {
+        cout << a << " " << b << endl;
+        return;
+      }
+    }
+  }
+  cout << -1;
+
+
+}
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  #ifdef _DEBUG
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  _SOLVE_();
+}
