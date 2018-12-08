ad6d7e0d97407d138a853e0f0b6e92adb9fcebeb
vlatkosh <vsarkovskii@gmail.com>
2018-12-08 13:21:10 +0100

    Create 954A - Diagonal Walking.cpp

---
+++ Programs/Codeforces/954A - Diagonal Walking.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class Key, class Compare = less<Key>> // find_by_order, order_of_key
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+
+
+
+void _SOLVE_() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int ans = n;
+  for (int i = 1; i < n; ++i) {
+    if (s[i] != s[i-1] && s[i-1] != 'D') {
+      --ans;
+      s[i] = 'D';
+    }
+  }
+  cout << ans << endl;
+
+
+
+
+
+}
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  #ifdef _DEBUG
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  _SOLVE_();
+}
