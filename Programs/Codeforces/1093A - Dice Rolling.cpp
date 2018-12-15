3c750a6249bd5d04113f4f86a3f9d554bc67a704
vlatkosh <vsarkovskii@gmail.com>
2018-12-15 19:12:37 +0100

    Create 1093A - Dice Rolling.cpp

---
+++ Programs/Codeforces/1093A - Dice Rolling.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class Key, class Compare = less<Key>> // find_by_order, order_of_key (for multiset: pair(val, time of insertion))
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+
+void _SOLVE_() {
+  int T;
+  cin >> T;
+  int data[102] = {};
+  while (T--) {
+    int x;
+    cin >> x;
+    int res = 0;
+    for (int y = 7; y >= 2; --y) {
+      while (x == y || x - y >= 2) {
+        x -= y;
+        ++res;
+      }
+    }
+    cout << res << endl;
+  }
+
+}
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  #ifdef _DEBUG
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  _SOLVE_();
+}
