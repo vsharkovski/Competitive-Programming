011b0edd69356073a09ad8260c3179abf5da1100
vlatkosh <vsarkovskii@gmail.com>
2018-12-16 09:23:36 +0100

    Create 1090M - The Pleasant Walk.cpp

---
+++ Programs/Codeforces/1090M - The Pleasant Walk.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class Key, class Compare = less<Key>> // find_by_order, order_of_key (for multiset: pair(val, time of insertion))
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+
+
+void _SOLVE_() {
+  int n, k;
+  cin >> n >> k;
+
+  vi a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+
+  int ans = 1, curr = 1;
+  for (int i = 1; i < n; ++i) {
+    if (a[i] == a[i-1]) {
+      curr = 1;
+    } else {
+      ++curr;
+    }
+    ans = max(ans, curr);
+  }
+
+  cout << ans << endl;
+}
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  #ifdef _DEBUG
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  _SOLVE_();
+}
