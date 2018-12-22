2ac38b78e21a04a271872c2911e833b2288fce4d
vlatkosh <vsarkovskii@gmail.com>
2018-12-22 13:08:41 +0100

    Create Srekjni broevi (Среќни).cpp

---
+++ Programs/Mendo/Srekjni broevi (Среќни).cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class Key, class Compare = less<Key>> // find_by_order, order_of_key (for multiset: pair(val, time of insertion))
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+
+
+
+void _SOLVE_() {
+  int k;
+  cin >> k;
+  ++k;
+  string ans = "";
+  while (k > 1) {
+    if ((k & 1) == 0) ans += '4';
+    else ans += '7';
+    k >>= 1;
+  }
+  reverse(ans.begin(), ans.end());
+  cout << ans;
+}
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  #ifdef _DEBUG
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  _SOLVE_();
+}
