0ac5792dabac0c74957820ef27043ea0338f0adb
vlatkosh <vsarkovskii@gmail.com>
2018-12-22 11:11:57 +0100

    Create 964B - Messages.cpp

---
+++ Programs/Codeforces/964B - Messages.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class Key, class Compare = less<Key>> // find_by_order, order_of_key (for multiset: pair(val, time of insertion))
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+
+
+
+void _SOLVE_() {
+  int n, A, B, C, T;
+  cin >> n >> A >> B >> C >> T;
+  int cash = 0;
+  for (int i = 0; i < n; ++i) {
+    int t;
+    cin >> t;
+    int o1 = A;
+    int o2 = A + (T - t) * (C - B);
+    cash += max(o1, o2);
+  }
+  cout << cash << endl;
+}
+
+
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  #ifdef _DEBUG
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  _SOLVE_();
+}
