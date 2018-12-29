1b8c6daebeca9c2904b0278f4c04335a06a6591d
vlatkosh <vsarkovskii@gmail.com>
2018-12-29 10:21:40 +0100

    Update 1096D - Easy Problem.cpp

--- Programs/Codeforces/1096D - Easy Problem.cpp
+++ Programs/Codeforces/1096D - Easy Problem.cpp
@@ -22,7 +22,7 @@ using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_no
 int n;
 string s;
 ll a[100005];
-ll dp[100005][1<<4];
+ll dp[100005][4];
 
 ll F(int i, int m) {
   if (dp[i][m] != -1) return dp[i][m];
@@ -30,17 +30,17 @@ ll F(int i, int m) {
   if (i == n) {
     res = 0;
   } else {
-    if (s[i] == 'h' && m == (1 << 0) - 1) {
-      res = min(res, F(i+1, m | (1 << 0)));
-      res = min(res, a[i] + F(i+1, m));
-    } else if (s[i] == 'a' && m == (1 << 1) -1 ) {
-      res = min(res, F(i+1, m | (1 << 1)));
-      res = min(res, a[i] + F(i+1, m));
-    } else if (s[i] == 'r' && m == (1 << 2) - 1) {
-      res = min(res, F(i+1, m | (1 << 2)));
-      res = min(res, a[i] + F(i+1, m));
-    } else if (s[i] == 'd' && m == (1 << 3) - 1) {
-      res = min(res, a[i] + F(i+1, m));
+    if (s[i] == 'h' && m == 0) {
+      res = min(res, F(i+1, 1));
+      res = min(res, a[i] + F(i+1, 0));
+    } else if (s[i] == 'a' && m == 1) {
+      res = min(res, F(i+1, 2));
+      res = min(res, a[i] + F(i+1, 1));
+    } else if (s[i] == 'r' && m == 2) {
+      res = min(res, F(i+1, 3));
+      res = min(res, a[i] + F(i+1, 2));
+    } else if (s[i] == 'd' && m == 3) {
+      res = min(res, a[i] + F(i+1, 3));
     } else {
       res = min(res, F(i+1, m));
     }
