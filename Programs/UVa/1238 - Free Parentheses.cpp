60343dde3caa2ab5ef5c1c6dd5c7610d6da08cd5
vlatkosh <vsarkovskii@gmail.com>
2019-01-02 12:04:31 +0100

    Update 1238 - Free Parentheses.cpp

--- Programs/UVa/1238 - Free Parentheses.cpp
+++ Programs/UVa/1238 - Free Parentheses.cpp
@@ -1,119 +1,87 @@
-#include <iostream>
-#include <iomanip>
-#include <algorithm>
-#include <utility>
-#include <cmath>
-#include <cstring>
-#include <functional>
-#include <iterator>
-#include <bitset>
-#include <vector>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <string>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <list>
-#include <stack>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-// printing
-#ifdef _DEBUG
-#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); _dbg(_it, args); }
-void _dbg(istream_iterator<string> it){++it;}
-template<typename T, typename... Args>
-void _dbg(istream_iterator<string> it, T a, Args... args){
-  if (*it=="'\\n'" || *it=="\"\\n\"" || *it=="endl") { cout << "\n"; }
-  else { cout << setw(8) << std::left << *it << " = " << setw(4) << std::right << a << "\n"; }
-  _dbg(++it, args...); }
-template<typename T>
-std::ostream& _containerprint(std::ostream &out, T const &val) { return (out << setw(4) << val << " "); }
-template<typename T1, typename T2>
-std::ostream& _containerprint(std::ostream &out, std::pair<T1, T2> const &val) { return (out << "{" << setw(4) << val.first << " " << setw(4) << val.second << "} "); }
-template<template<typename, typename...> class TT, typename... Args>
-std::ostream& operator<<(std::ostream &out, const TT<Args...> &cont) { for(auto&& elem : cont) { _containerprint(out, elem); } return out; }
-std::ostream& operator<<(std::ostream &out, const std::string &s) { return operator << <char>(out, s); }
-#endif
-// other
-#define endl '\n'
-#define all(v) begin(v), end(v)
+using namespace __gnu_pbds;
 typedef long long ll;
 typedef long double ld;
-typedef std::pair<int, int> ii;
-typedef std::vector<ii> vii;
-typedef std::vector<int> vi;
-const double EPS = 1e-9;
-const int mod = 1e9+7;
-template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
-template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class Key, class Compare = less<Key>> // find_by_order, order_of_key (for multiset: pair(val, time of insertion))
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
 
 
 
+const int OFS = 3001;
 
+int N, ans;
+int x[31];
+bool used[6002];
+bool dp[31][31][6002];
 
-const int OFS = 3000;
+// we only add parenthesis before negative numbers
+// d = depth of parenthesis (if d is odd, signs are flipped)
 
-int n;
-int num[32];
-int sgn[32];
-
-bool used[6100];
-bool visited[32][32][6100];
-
-void F(int i, int p, int x) {
-  if (visited[i][p][OFS+x]) return;
-  visited[i][p][OFS+x] = 1;
-  if (i == n) {
-    used[OFS+x] = 1;
+void dfs(int i, int d, int s) {
+  if (dp[i][d][s]) {
     return;
   }
-  x += num[i] * sgn[i] * (p % 2 == 0 ? 1 : -1);
-  F(i+1, p, x);
-  if (sgn[i] == -1) F(i+1, p+1, x);
-  if (p > 0)        F(i+1, p-1, x);
+  dp[i][d][s] = true;
+  if (i == N) {
+    if (used[OFS+s] == false) {
+      ++ans;
+      used[OFS+s] = true;
+    }
+    return;
+  }
+  if (d % 2 == 0) {
+    s += x[i];
+  } else {
+    s -= x[i];
+  }
+  dfs(i + 1, d, s); // continue without adding parenthesis
+  if (x[i] < 0) {
+    dfs(i + 1, d + 1, s); // add open parenthesis
+  }
+  if (d > 0) {
+    dfs(i + 1, d - 1, s); // add close parenthesis
+  }
 }
 
-void Solve() {
-  stringstream ss;
-  string line;
-  char ch;
-  int val;
-  while (getline(cin, line)) {
-    ss.clear();
-    ss << line;
-    ss >> val;
-    num[0] = val;
-    sgn[0] = 1;
-    n = 1;
-    while (ss >> ch >> val) {
-      sgn[n] = ch == '+' ? 1 : -1;
-      num[n] = val;
-      ++n;
+void Solution() {
+  string s;
+  while (getline(cin, s)) {
+    N = 0;
+    stringstream ss;
+    char ch;
+    int y;
+    s = "+ " + s;
+    ss << s;
+    while (ss >> ch >> y) {
+      if (ch == '-') y = -y;
+      x[N] = y;
+      ++N;
     }
     memset(used, 0, sizeof(used));
-    memset(visited, 0, sizeof(visited));
-    F(0, 0, 0);
-    int cnt = 0;
-    for (int x = -3000; x <= 3000; ++x)
-      if (used[OFS+x]) ++cnt;
-    cout << cnt << endl;
+    memset(dp, 0, sizeof(dp));
+    ans = 0;
+    dfs(0, 0, 0);
+    cout << ans << '\n';
   }
-
-
-
-
 }
 
-
-
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   #ifdef _DEBUG
-  std::freopen("in.txt", "r", stdin);
-  std::freopen("out.txt", "w", stdout);
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
   #endif
-  Solve();
+  Solution();
 }
