0f77f77a9299a9c557aa85de3327c861d9595375
vlatkosh <vsarkovskii@gmail.com>
2019-01-02 11:18:43 +0100

    Update 1231 - ACORN.cpp

--- Programs/UVa/1231 - ACORN.cpp
+++ Programs/UVa/1231 - ACORN.cpp
@@ -1,104 +1,78 @@
-#include <iostream>
-#include <iomanip>
-#include <algorithm>
-#include <utility>
-#include <cmath>
-#include <cstring>
-#include <functional>
-#include <iterator>
-#include <bitset>
-#include <vector>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <string>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <list>
-#include <stack>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-// printing
-#ifdef _DEBUG
-#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); _dbg(_it, args); }
-void _dbg(istream_iterator<string> it){++it;}
-template<typename T, typename... Args>
-void _dbg(istream_iterator<string> it, T a, Args... args){
-  if (*it=="'\\n'" || *it=="\"\\n\"" || *it=="endl") { cout << "\n"; }
-  else { cout << setw(8) << std::left << *it << " = " << setw(4) << std::right << a << "\n"; }
-  _dbg(++it, args...); }
-template<typename T>
-std::ostream& _containerprint(std::ostream &out, T const &val) { return (out << setw(4) << val << " "); }
-template<typename T1, typename T2>
-std::ostream& _containerprint(std::ostream &out, std::pair<T1, T2> const &val) { return (out << "{" << setw(4) << val.first << " " << setw(4) << val.second << "} "); }
-template<template<typename, typename...> class TT, typename... Args>
-std::ostream& operator<<(std::ostream &out, const TT<Args...> &cont) { for(auto&& elem : cont) { _containerprint(out, elem); } return out; }
-std::ostream& operator<<(std::ostream &out, const std::string &s) { return operator << <char>(out, s); }
-#endif
-// other
-#define endl '\n'
-#define all(v) begin(v), end(v)
+using namespace __gnu_pbds;
 typedef long long ll;
 typedef long double ld;
-typedef std::pair<int, int> ii;
-typedef std::vector<ii> vii;
-typedef std::vector<int> vi;
-const double EPS = 1e-9;
-const int mod = 1e9+7;
-template<typename T> T gcd(T a, T b){T c; while(b){c=b; b=a%b; a=c;} return a;}
-template<typename T> T powmod(T a, T b){T res = 1; a %= mod; while(b){if(b&1)res=res*(a%mod); a=a*(a%mod); b>>=1;} return res;}
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+template <class Key, class Compare = less<Key>> // find_by_order, order_of_key (for multiset: pair(val, time of insertion))
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
 
 
 
 
 
 
-
-int t, h, f;
-int acorns[2001][2001];
-int dp[2001];
-
-void Solve() {
-  cin >> t >> h >> f;
-  memset(acorns, 0, sizeof(acorns));
-  for (int i = 0; i < t; ++i) {
-    int a;
-    cin >> a;
-    while (a--) {
-      int n;
-      cin >> n;
-      ++acorns[i][n];
-    }
-  }
-  dp[h] = 0;
-  for (int i = 0; i < t; ++i)
-    dp[h] = max(dp[h], acorns[i][h]);
-  for (int n = h-1; n >= 0; --n) {
-    dp[n] = 0;
-    for (int i = 0; i < t; ++i) {
-      acorns[i][n] += max(acorns[i][n+1],
-                          (n+f <= h) ? dp[n+f] : 0);
-      dp[n] = max(dp[n], acorns[i][n]);
-    }
-  }
-  cout << dp[0] << endl;
-}
-
-void Input() {
+void Solution() {
   int T;
   cin >> T;
   while (T--) {
-    Solve();
+
+    int nt, mh, f;
+    cin >> nt >> mh >> f;
+
+    vector<vector<int>> acorn(nt+2, vector<int>(mh+2, 0));
+
+    for (int i = 0; i < nt; ++i) {
+      int a, h;
+      cin >> a;
+      while (a--) {
+        cin >> h;
+        ++acorn[i][h];
+      }
+    }
+
+    for (int h = mh; h >= 0; --h) {
+      int bestifjumped = 0;
+      // you could replace the following loop with a dp table
+      // dp[h] = bestifjumped at height h
+      // dp[h] = max( acorn[i][h] ), i = 0, 1, ..., nt-1
+      for (int i = 0; i < nt; ++i) {
+        if (h + f <= mh) {
+          int ifjumped = acorn[i][h+f];
+          bestifjumped = max(bestifjumped, ifjumped);
+        }
+      }
+      for (int i = 0; i < nt; ++i) {
+        int ifstood = acorn[i][h+1];
+        acorn[i][h] += max(ifstood, bestifjumped);
+      }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < nt; ++i) {
+      ans = max(ans, acorn[i][0]);
+    }
+
+    cout << ans<< '\n';
+
   }
 }
 
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   #ifdef _DEBUG
-  std::freopen("in.txt", "r", stdin);
-  std::freopen("out.txt", "w", stdout);
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
   #endif
-  Input();
+  Solution();
 }
