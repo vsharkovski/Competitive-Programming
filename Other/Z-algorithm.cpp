66babd9c5824e7a6706d84de1dc8f5e061b52ccf
vlatkosh <vsarkovskii@gmail.com>
2019-01-10 15:38:40 +0100

    Create Z-algorithm.cpp

---
+++ Other/Z-algorithm.cpp
@@ -0,0 +1,14 @@
+vector<int> makez(string s) {
+  int n = s.length();
+  vector<int> z(n);
+  int l = 0, r = 0;
+  for (int i = 1; i < n; ++i) {
+    z[i] = max(0, min(z[i - l], r - i + 1));
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      l = i;
+      r = i + z[i];
+      ++z[i];
+    }
+  }
+  return z;
+}
