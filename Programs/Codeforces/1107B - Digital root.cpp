20c357f0356cbef211bf1fadcbbebbc8522dd709
vlatkosh <vsarkovskii@gmail.com>
2019-01-27 10:06:32 +0100

    Create 1107B - Digital root.cpp

---
+++ Programs/Codeforces/1107B - Digital root.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+template<class Key, class Compare = less<Key>>
+using Tree = tree<Key, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+
+void Main() {
+  int q;
+  cin >> q;
+  while (q--) {
+    ll k, x;
+    cin >> k >> x;
+    cout << 9*(k-1) + x << "\n";
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  #ifdef _DEBUG
+//  freopen("in.txt", "r", stdin);
+//  freopen("out.txt", "w", stdout);
+  #endif
+  Main();
+  return 0;
+}
