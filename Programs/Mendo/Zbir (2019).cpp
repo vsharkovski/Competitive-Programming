733b8f320efec7884760d7d965fa3a41d2930dce
vlatkosh <vsarkovskii@gmail.com>
2019-03-13 09:07:32 +0100

    Create Zbir (2019).cpp

---
+++ Programs/Mendo/Zbir (2019).cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+
+
+
+void Main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c*c << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    #ifndef _DEBUG
+    #endif
+    Main();
+    return 0;
+}
