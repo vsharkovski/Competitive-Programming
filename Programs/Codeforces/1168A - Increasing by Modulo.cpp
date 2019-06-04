347897398d03248d62250562611a7d91361139d8
vlatkosh <vsarkovskii@gmail.com>
2019-06-04 10:03:50 +0200

    Update 1168A - Increasing by Modulo.cpp

--- Programs/Codeforces/1168A - Increasing by Modulo.cpp
+++ Programs/Codeforces/1168A - Increasing by Modulo.cpp
@@ -8,25 +8,25 @@ typedef pair<int, int> pii;
 const int maxn = 300005;
 
 int n, m;
-int a[maxn], b[maxn];
+int a[maxn];
 
 bool possible(int limit) {
-    for (int i = 0; i <= n; ++i) {
-        b[i] = a[i];
-    }
-    for (int i = 1; i <= n; ++i) {
-        if (b[i-1] > b[i]) {
-            if (b[i-1] - b[i] <= limit) {
-                b[i] = b[i-1];
+    int pr = 0, cr;
+    for (int i = 0; i < n; ++i) {
+        cr = a[i];
+        if (pr > cr) {
+            if (pr - cr <= limit) {
+                cr = pr;
             } else {
                 return false;
             }
-        } else if (b[i-1] < b[i]) {
-            int needed = m - (b[i] - b[i-1]);
+        } else if (pr < cr) {
+            int needed = m - (cr - pr);
             if (needed <= limit) {
-                b[i] = b[i-1];
+                cr = pr;
             }
         }
+        pr = cr;
     }
     return true;
 }
@@ -35,8 +35,7 @@ int main() {
     ios::sync_with_stdio(false); cin.tie(0);
 
     cin >> n >> m;
-    a[0] = 0;
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
 
