cff987fa935e7d5cdb455c881ae4bafe756927bb
vlatkosh <vsarkovskii@gmail.com>
2019-07-08 12:53:39 +0200

    Update Output-only.cpp

--- Other/Templates/Output-only.cpp
+++ Other/Templates/Output-only.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 
 
+
 void solve(ifstream& in, ofstream& out) {
 
 }
@@ -13,16 +14,16 @@ int main() {
         stringstream ss;
         ss << setfill('0') << setw(2) << i;
         string test_name = ss.str();
-        string in_full = "input/" + test_name + ".in";
+        string in_full = "tests/" + test_name + ".in";
         string out_full = "output/" + test_name + ".out";
         ifstream in(in_full);
-        ofstream out(out_full);
         if (in.good()) {
+            ofstream out(out_full);
             solve(in, out);
+            in.close();
+            out.close();
         } else {
             cout << "Problem reading " << in_full << "\n";
         }
-        in.close();
-        out.close();
     }
 }
