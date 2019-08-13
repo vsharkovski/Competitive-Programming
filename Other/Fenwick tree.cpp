67666976be8e4ddf0bb9676144ef74eb73f79c3e
vlatkosh <vsarkovskii@gmail.com>
2019-08-13 15:37:56 +0200

    Update Fenwick tree.cpp

--- Other/Fenwick tree.cpp
+++ Other/Fenwick tree.cpp
@@ -1,59 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-
-
-// NOTE; 1-indexed
-
-const int maxn = 100005;
-
-
-int n;
-int a[maxn];
-int ft[maxn];
-
-void update(int v, int x) {
-    for (; v <= n; v += v & -v) {
-        ft[v] += x;
+struct fenwick {
+    int n;
+    vector<int> tree;
+    fenwick(int n) : n(n) {
+        tree.assign(n+1, 0);
     }
-}
-
-int sum(int v) {
-    int res = 0;
-    for (; v >= 1; v -= v & -v) {
-        res += ft[v];
-    }
-    return res;
-}
-
-int sum(int l, int r) {
-    return sum(r) - sum(l-1);
-}
-
-void Main() {
-    cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        cin >> a[i];
-        update(i, a[i]);
-    }
-    while (true) {
-        int type;
-        cin >> type;
-        if (type == 1) {
-            int v, x;
-            cin >> v >> x;
-            update(v, x);
-        } else if (type == 2) {
-            int l, r;
-            cin >> l >> r;
-            cout << sum(l, r) << '\n';
+    void update(int pos, int val) {
+        while (pos <= n) {
+            tree[pos] += val;
+            pos += pos & -pos;
         }
     }
-}
-
-int main() {
-//    ios::sync_with_stdio(false);
-//    cin.tie(0);
-    Main();
-}
+    int query(int pos) {
+        int res = 0;
+        while (pos >= 1) {
+            res += tree[pos];
+            pos -= pos & -pos;
+        }
+        return res;
+    }
+    int query(int l, int r) {
+        return query(r) - query(l-1);
+    }
+};
