0cc5251038d9a83a8778b6ba1ca1cd679e548d9c
vlatkosh <vsarkovskii@gmail.com>
2019-08-13 15:34:19 +0200

    Update Disjoint Set Union.cpp

--- Other/Disjoint Set Union.cpp
+++ Other/Disjoint Set Union.cpp
@@ -1,28 +1,20 @@
-class DSU {
-public:
-  vector<int> p, rank;
-  int n;
-  DSU(int _n) : n(_n) {
-    rank.assign(n, 0);
-    p.resize(n);
-    iota(p.begin(), p.end(), 0);
-  }
-  inline int get(int x) {
-    return (p[x] == x) ? x : (p[x] = get(p[x]));
-  }
-  inline bool unite(int x, int y) {
-    x = get(x), y = get(y);
-    if (x != y) {
-      if (rank[x] > rank[y]) {
-        p[y] = x;
-      } else {
-        p[x] = y;
-        if (rank[x] == rank[y]) {
-          ++rank[y];
-        }
-      }
-      return true;
+struct DSU {
+    int n;
+    vector<int> p, rank;
+    DSU(int n) : n(n) {
+        rank.assign(n, 0);
+        p.resize(n);
+        iota(p.begin(), p.end(), 0);
     }
-    return false;
-  }
+    inline int get(int x) {
+        return p[x] == x ? x : (p[x] = get(p[x]));
+    }
+    inline bool unite(int x, int y) {
+        x = get(x), y = get(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y]) swap(x, y);
+        if (rank[x] == rank[y]) ++rank[x];
+        p[y] = x;
+        return true;
+    }
 };
