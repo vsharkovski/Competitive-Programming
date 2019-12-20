1f3ecf48935138bf204ae7cf9280434d10f27222
vlatkosh <vsarkovskii@gmail.com>
2019-12-20 20:59:58 +0100

    Update Osumdelivi.cpp

--- Programs/Mendo/Osumdelivi.cpp
+++ Programs/Mendo/Osumdelivi.cpp
@@ -1,36 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
 
 const int mod = 1e9 + 7;
 const int m = 8;
 
-const int N = 200010;
-
-int n;
-int a[N];
-int dp[N][m];
-
-int dfs(int i, int j) {
-	int& res = dp[i][j];
-	if (res != -1) return res;
-	if (i == n) return res = (j == 0);
-	res = 0;
-	res = (res + dfs(i+1, j)) % mod;
-	res = (res + dfs(i+1, (10*j + a[i]) % m)) % mod;
-	return res;
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
+	int n;
 	cin >> n;
+	vector<int> cur(m), nxt(m);
+	cur[0] = 1;
 	for (int i = 0; i < n; ++i) {
 		char ch;
 		cin >> ch;
-		a[i] = ch - '0';
+		int a = ch - '0';
+		nxt.assign(m, 0);
+		for (int j = 0; j < m; ++j) {
+			nxt[j] = (nxt[j] + cur[j]) % mod;
+			nxt[(10*j + a)%m] = (nxt[(10*j + a)%m] + cur[j]) % mod;
+		}
+		swap(cur, nxt);
 	}
-	memset(dp, -1, sizeof(dp));
-	cout << dfs(0, 0)-1 << endl;
+	cout << cur[0]-1 << endl;
 }
