65ff7d08da4915a67cdc58312caa615a70835a9e
vlatkosh <vsarkovskii@gmail.com>
2020-01-06 09:15:30 +0100

    Update and rename Computation geometry stuff.cpp to Computational geometry stuff.cpp

--- Other/Computational geometry stuff.cpp
+++ Other/Computational geometry stuff.cpp
@@ -20,9 +20,5 @@ Sort points counter-clockwise
 sort(points.begin(), points.end(), [&](const pii& a, const pii& b) {
     int aUp = (a.second > 0) || (a.second == 0 && a.first > 0);
     int bUp = (b.second > 0) || (b.second == 0 && b.first > 0);
-    if (aUp && !bUp) return true;
-    if (!aUp && bUp) return false;
-    ll vp = prod(a, b);
-    if (vp > 0) return true;
-    return false;
+    return aUp == bUp ? prod(a, b) > 0 : aUp < bUp;
 });
