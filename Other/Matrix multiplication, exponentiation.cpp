6eb7c352bf52bc34a8b5090e469f8f7e582aa9c4
vlatkosh <vsarkovskii@gmail.com>
2020-01-12 16:58:04 +0100

    Update Matrix multiplication, exponentiation.cpp

--- Other/Matrix multiplication, exponentiation.cpp
+++ Other/Matrix multiplication, exponentiation.cpp
@@ -23,3 +23,46 @@ matrix matPow(matrix& a, int b) {
     }
     return res;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+const int M = 100;
+
+struct matrix {
+	int r, c;
+	ll m[M][M];
+	matrix(int r, int c) : r(r), c(c) {
+		memset(m, 0, sizeof(m));
+	}
+	matrix& operator = (const matrix& o) {
+		r = o.r, c = o.c;
+		for (int i = 0; i < r; ++i)
+			for (int j = 0; j < c; ++j)
+				m[i][j] = o.m[i][j];
+		return *this;
+	}
+};
+
+void matmul(matrix& a, matrix& b, matrix& res) {
+	int p = a.r, q = a.c, r = b.c;
+	matrix c(p, r);
+	for (int i = 0; i < p; ++i)
+		for (int k = 0; k < q; ++k)
+			for (int j = 0; j < r; ++j)
+				c.m[i][j] = (c.m[i][j] + a.m[i][k]*b.m[k][j]) % mod;
+	res = c;
+}
+
+void matpow(matrix& a, matrix& res_real, int pwr) {
+	matrix res(a.r, a.r);
+	for (int i = 0; i < a.r; ++i) res.m[i][i] = 1;
+	while (pwr) {
+		if (pwr & 1) {
+			matmul(res, a, res);
+		}
+		matmul(a, a, a);
+		pwr /= 2;
+	}
+	res_real = res;
+}
