855df96c2c76c4c7c770850c55876a6458274806
vlatkosh <vsarkovskii@gmail.com>
2020-03-06 15:41:08 +0100

    Update Articulation point, bridge.cpp

--- Other/Articulation point, bridge.cpp
+++ Other/Articulation point, bridge.cpp
@@ -1,56 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn = 100010;
+const int N = 100100;
 
 int n, m;
-vector<int> adj[maxn];
+vector<int> adj[N];
 
 int timer;
-int tin[maxn]; // time entered in dfs
-int low[maxn]; // min{tin[u], tin[p] (u-p is back edge), low[v] (u-v is tree edge)}
+int tin[N]; // time entered with dfs
+int low[N]; // min{tin[u], tin[p] (u-p is back edge), low[v] (u-v is tree edge)}
 
-void artpb(int u, int p) {
-    tin[u] = low[u] = timer++;
-    int children = 0;
-    for (int v : adj[u]) {
-        if (v == p) {
-            // parent
-            continue;
-        } else if (tin[v] != -1) {
-            // back edge
-            low[u] = min(low[u], tin[v]);
-        } else {
-            // tree edge
-            ++children;
-            artpb(v, u);
-            low[u] = min(low[u], low[v]);
-            if (low[v] >= tin[u] && p != -1) {
-                // u is articulation point
-                cout << "cut point: " << u << endl;
-            }
-            if (low[v] > tin[u]) {
-                // (u, v) is articulation bridge
-                cout << "bridge: " << u << "-" << v << endl;
-            }
-        }
-    }
-    if (p == -1 && children > 1) {
-        // u (root) is also articulation point
-        cout << "cut point: " << u << endl;
-    }
+void dfs(int node, int parent) {
+	tin[node] = low[node] = timer++;
+	int children = 0;
+	for (int v : adj[node]) {
+		if (tin[v] == -1) {
+			++children;
+			dfs(v, node);
+			low[node] = min(low[node], low[v]);
+			if (low[v] > num[node]) {
+				// this edge is bridge
+			}
+			if (parent != -1 && low[v] >= num[node]) {
+				// node is cut point
+			}
+		} else if (v != parent) {
+			// back edge
+			low[node] = min(low[node], tin[v]);
+		}
+	}
+	if (parent == -1 && children > 1) {
+		// node is cut point
+	}
 }
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0);
-    cin >> n >> m;
-    for (int i = 0; i < m; ++i) {
-        int a, b;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    timer = 0;
-    memset(tin, -1, sizeof(tin));
-    artpb(1, -1);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < m; ++i) {
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	for (int i = 1; i <= n; ++i) {
+		tin[i] = -1;
+	}
+	timer = 0;
+	for (int i = 1; i <= n; ++i) {
+		if (tin[i] == -1) {
+			dfs(i, -1);
+		}
+	}
 }
