b668dc6bf3e91496ad27d0060410b946124a468d
vlatkosh <vsarkovskii@gmail.com>
2020-03-06 16:22:15 +0100

    Update Tarjan SCC.cpp

--- Other/Tarjan SCC.cpp
+++ Other/Tarjan SCC.cpp
@@ -1,65 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
 
+const int N = 100100;
 
+int n, m;
+vector<int> adj[N];
 
-int n;
-vector<vector<int>> adj;
+int timer;
+int tin[N], low[N];
+bool instack[N];
+vector<int> stk;
 
-vector<int> num, low, instk, stk;
-int calls;
-
-void tarjan(int u) {
-    num[u] = low[u] = calls++;
-    instk[u] = 1;
-    stk.push_back(u);
-    for (int v : adj[u]) {
-        if (num[v] == -1) {
-            tarjan(v);
-        }
-        if (instk[v]) {
-            low[u] = min(low[u], low[v]);
-        }
-    }
-    if (low[u] == num[u]) {
-        cout << "SCC: ";
-        while (true) {
-            int v = stk.back();
-            stk.pop_back();
-            instk[v] = 0;
-            // v is in SCC
-            cout << v << " ";
-            if (v == u) break;
-        }
-        cout << "\n";
-    }
+void dfs(int u, int p) {
+	tin[u] = low[u] = timer++;
+	instack[u] = true;
+	stk.push_back(u);
+	for (int v : adj[u]) {
+		if (tin[v] == -1) {
+			dfs(v, u);
+		}
+		if (instack[v]) {
+			low[u] = min(low[u], low[v]);
+		}
+	}
+	if (tin[u] == low[u]) {
+		cout << "SCC: ";
+		while (true) {
+			int v = stk.back();
+			stk.pop_back();
+			cout << v << ' ';
+			if (v == u) break;
+		}
+		cout << '\n';
+	}
 }
 
-void Main() {
-    cin >> n;
-    adj.assign(n+1, vector<int>());
-    int m;
-    cin >> m;
-    while (m--) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-    }
-    num.assign(n+1, -1);
-    low.resize(n);
-    instk.assign(n+1, 0);
-    calls = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (num[i] == -1) {
-            tarjan(i);
-        }
-    }
-}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    Main();
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < m; ++i) {
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+	}
+	memset(tin, -1, sizeof(tin));
+	timer = 0;
+	for (int i = 1; i <= n; ++i) {
+		if (tin[i] == -1) {
+			dfs(i, -1);
+		}
+	}
 }
+
+/*
+sample input:
+8 10
+1 2
+2 3
+3 4
+4 1
+2 5
+5 6
+6 7
+7 5
+8 3
+8 6
+*/
