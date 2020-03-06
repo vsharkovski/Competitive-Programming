9adeafc0fac116b0756517572fedfab2ba5711af
vlatkosh <vsarkovskii@gmail.com>
2020-03-06 16:15:58 +0100

    Update Lowest Common Ancestor.cpp

--- Other/Lowest Common Ancestor.cpp
+++ Other/Lowest Common Ancestor.cpp
@@ -1,72 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
 
-const int maxn = 100010;
-const int maxlog = 17;
+const int N = 100100;
+const int K = 17;
 
 int n;
-vector<int> adj[maxn];
+vector<int> adj[N];
 
-int depth[maxn];
-int P[maxn][maxlog];
+int depth[N];
+int P[N][K];
 
-void dfs(int u) {
-    for (int v : adj[u]) {
-        if (P[v][0] == -1) {
-            P[v][0] = u;
-            depth[v] = depth[u] + 1;
-            dfs(v);
-        }
-    }
-}
-
-void preprocessLCA() {
-    for (int j = 0; j < maxlog; ++j) {
-        for (int i = 1; i <= n; ++i) {
-            P[i][j] = -1;
-        }
-    }
-    depth[1] = 0;
-    dfs(1);
-    for (int j = 1; j < maxlog; ++j) {
-        for (int i = 1; i <= n; ++i) {
-            if (P[i][j-1] != -1) {
-                P[i][j] = P[P[i][j-1]][j-1];
-            }
-        }
-    }
+void dfs(int node, int parent, int dpth) {
+	P[node][0] = parent;
+	depth[node] = dpth;
+	for (int v : adj[node]) {
+		if (v == parent) continue;
+		dfs(v, node, dpth+1);
+	}
 }
 
 int LCA(int a, int b) {
-    if (depth[a] < depth[b]) swap(a, b);
-    for (int j = maxlog-1; j >= 0; --j) {
-        if (depth[a] - (1 << j) >= depth[b]) {
-            a = P[a][j];
-        }
-    }
-    if (a == b) return a;
-    for (int j = maxlog-1; j >= 0; --j) {
-        if (P[a][j] != -1 && P[a][j] != P[b][j]) {
-            a = P[a][j];
-            b = P[b][j];
-        }
-    }
-    return P[a][0];
+	if (depth[a] < depth[b]) swap(a, b);
+	for (int j = K-1; j >= 0; --j) {
+		if (depth[a] - (1<<j) >= depth[b]) {
+			a = P[a][j];
+		}
+	}
+	if (a == b) return a;
+	for (int j = K-1; j >= 0; --j) {
+		if (P[a][j] != -1 && P[a][j] != P[b][j]) {
+			a = P[a][j];
+			b = P[b][j];
+		}
+	}
+	return P[a][0];
 }
 
 int main() {
-    cin >> n;
-    for (int i = 0; i < n-1; ++i) {
-        int a, b;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    preprocessLCA();
-    while (true) {
-        cout << "Enter 2 numbers" << endl;
-        int a, b;
-        cin >> a >> b;
-        cout << "LCA(" << a << ", " << b << ") = " << LCA(a, b) << endl;
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n;
+	for (int i = 0; i < n-1; ++i) {
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	dfs(1, -1, 0);
+	for (int j = 1; j < K; ++j) {
+		for (int i = 1; i <= n; ++i) {
+			if (P[i][j-1] == -1) {
+				P[i][j] = -1;
+			} else {
+				P[i][j] = P[ P[i][j-1] ][j-1];
+			}
+		}
+	}
+	for (int i = 1; i <= n; ++i) {
+		for (int j = i+1; j <= n; ++j) {
+			cout << "LCA(" << i << ", " << j << ") = " << LCA(i, j) << '\n';
+		}
+	}
 }
+
+/*
+sample input:
+10
+1 2
+2 3
+2 4
+4 5
+5 6
+5 7
+4 8
+8 9
+8 10
+*/
