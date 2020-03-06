39932e021aa573988833dbbba31843057477db36
vlatkosh <vsarkovskii@gmail.com>
2020-03-06 13:11:33 +0100

    Create nocross.cpp

---
+++ Programs/USACO/nocross.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int D = 4;
+
+struct segtree {
+	int base;
+	vector<int> tree;
+	segtree(int size) {
+		base = 1;
+		while (base < size) base *= 2;
+		tree.resize(2*base + 2);
+	}
+	void update(int pos, int val) {
+		update(1, 0, base-1, pos, val);
+	}
+	void update(int v, int tl, int tr, int pos, int val) {
+		if (tl == tr) {
+			tree[v] = max(tree[v], val);
+			return;
+		}
+		int tm = (tl + tr) / 2;
+		if (pos <= tm) {
+			update(2*v, tl, tm, pos, val);
+		} else {
+			update(2*v+1, tm+1, tr, pos, val);
+		}
+		tree[v] = max(tree[2*v], tree[2*v+1]);
+	}
+	int query(int l, int r) {
+		return query(1, 0, base-1, l, r);
+	}
+	int query(int v, int tl, int tr, int l, int r) {
+		if (l > r) return 0;
+		if (l <= tl && tr <= r) return tree[v];
+		int tm = (tl + tr) / 2;
+		return	max(query(2*v, tl, tm, l, min(r, tm)),
+					query(2*v+1, tm+1, tr, max(l, tm+1), r));
+	}
+};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	#ifndef _DEBUG
+	freopen("nocross.in", "r", stdin);
+	freopen("nocross.out", "w", stdout);
+    #endif
+	int n;
+	cin >> n;
+	vector<int> a(n), pos(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+		--a[i];
+	}
+	for (int i = 0; i < n; ++i) {
+		int b;
+		cin >> b;
+		--b;
+		pos[b] = i;
+	}
+	segtree tree(n+1);
+	for (int i = 0; i < n; ++i) {
+		vector<int> pot;
+		for (int x = max(0, a[i]-D); x <= min(n-1, a[i]+D); ++x) {
+			pot.push_back(pos[x]);
+		}
+		sort(pot.rbegin(), pot.rend());
+		for (int j : pot) {
+			int dp = 1 + tree.query(0, j-1);
+			tree.update(j, dp);
+		}
+	}
+	cout << tree.query(0, n-1) << '\n';
+}
