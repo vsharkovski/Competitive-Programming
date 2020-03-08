0c60482f8b19ca9740304880c1f4c2f975ae5549
vlatkosh <vsarkovskii@gmail.com>
2020-03-08 11:52:45 +0100

    Create Sebeobjasnuvacki broj.cpp

---
+++ Programs/Mendo/Sebeobjasnuvacki broj.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+ 
+int main() {
+    string ans[11] = {"0", "0", "0", "0", "1210", "21200", "0", "3211000", "42101000", "521001000", "6210001000"};
+    int k;
+    cin >> k;
+    cout << ans[k] << '\n';
+    return 0;
+}
