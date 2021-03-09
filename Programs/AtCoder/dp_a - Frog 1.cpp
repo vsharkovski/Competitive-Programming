b4aea51c715cd41c488828ec8e38a9fb81abc947
vlatkosh <vsarkovskii@gmail.com>
2021-03-09 18:31:50 +0100

    Update dp_a - Frog 1.cpp

--- Programs/AtCoder/dp_a - Frog 1.cpp
+++ Programs/AtCoder/dp_a - Frog 1.cpp
@@ -1,41 +1,22 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-//using namespace __gnu_pbds;
 typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-template <class Key, class Compare = less<Key>>
-using Tree = __gnu_pbds::tree<Key, __gnu_pbds::null_type, Compare, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-
-
-
-
-
-void Main() {
-  int n;
-  cin >> n;
-  vi h(n), dp(n);
-  for (int i = 0; i < n; ++i) {
-    cin >> h[i];
-  }
-  dp[0] = 0;
-  dp[1] = abs(h[0] - h[1]);
-  for (int i = 2; i < n; ++i) {
-    dp[i] = min(dp[i-2] + abs(h[i-2] - h[i]), dp[i-1] + abs(h[i-1] - h[i]));
-  }
-  cout << dp[n-1] << '\n';
-}
-
+typedef pair<int, int> pii;
 
 int main() {
-  ios::sync_with_stdio(false);
-//  cin.tie(nullptr);
-  #ifdef _DEBUG
-//  freopen("in.txt", "r", stdin);
-//  freopen("out.txt", "w", stdout);
-  #endif
-  Main();
-  return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<int> h(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> h[i];
+	}
+	vector<int> dp(n, 1e9);
+	dp[0] = 0;
+	for (int i = 0; i < n; ++i) {
+		if (i+1 < n) dp[i+1] = min(dp[i+1], dp[i]+abs(h[i+1]-h[i]));
+		if (i+2 < n) dp[i+2] = min(dp[i+2], dp[i]+abs(h[i+2]-h[i]));
+	}
+	cout << dp[n-1] << '\n';
 }
