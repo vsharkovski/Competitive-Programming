25804590c347b4282ca1579ec93da7e4b68b4a4d
vlatkosh <vsarkovskii@gmail.com>
2021-03-09 18:43:01 +0100

    Update dp_c - Vacation.cpp

--- Programs/AtCoder/dp_c - Vacation.cpp
+++ Programs/AtCoder/dp_c - Vacation.cpp
@@ -1,57 +1,30 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-//using namespace __gnu_pbds;
 typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-template <class Key, class Compare = less<Key>>
-using Tree = __gnu_pbds::tree<Key, __gnu_pbds::null_type, Compare, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-
-
-
-
-
-void Main() {
-  int n;
-  cin >> n;
-  static int a[100000][3];
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      cin >> a[i][j];
-    }
-  }
-  static int dp[100000][3];
-  for (int j = 0; j < 3; ++j) {
-    dp[0][j] = a[0][j];
-  }
-  for (int i = 1; i < n; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      int bestprev = 0;
-      for (int k = 0; k < 3; ++k) {
-        if (k != j) {
-          bestprev = max(bestprev, dp[i-1][k]);
-        }
-      }
-      dp[i][j] = max(dp[i][j], bestprev + a[i][j]);
-    }
-  }
-  int ans = 0;
-  for (int j = 0; j < 3; ++j) {
-    ans = max(ans, dp[n-1][j]);
-  }
-  cout << ans << '\n';
-}
-
+typedef pair<int, int> pii;
 
 int main() {
-  ios::sync_with_stdio(false);
-//  cin.tie(nullptr);
-  #ifdef _DEBUG
-//  freopen("in.txt", "r", stdin);
-//  freopen("out.txt", "w", stdout);
-  #endif
-  Main();
-  return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	const int m = 3;
+	vector<int> cur(m), nxt(m);
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			int a;
+			cin >> a;
+			for (int k = 0; k < m; ++k) {
+				if (k == j) continue;
+				nxt[j] = max(nxt[j], cur[k] + a);
+			}
+		}
+		swap(cur, nxt);
+		nxt.assign(m, 0);
+	}
+	int ans = 0;
+	for (int i = 0; i < m; ++i) {
+		ans = max(ans, cur[i]);
+	}
+	cout << ans << '\n';
 }
