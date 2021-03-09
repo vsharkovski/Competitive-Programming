7efcec61c8785c54047c94947b8a7baa4c20d48f
vlatkosh <vsarkovskii@gmail.com>
2021-03-09 18:22:15 +0100

    Update dp_e - Knapsack 2.cpp

--- Programs/AtCoder/dp_e - Knapsack 2.cpp
+++ Programs/AtCoder/dp_e - Knapsack 2.cpp
@@ -1,43 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-
-
-
-
-
-
-
-void Main() {
-    int N;
-    ll W;
-    cin >> N >> W;
-    vector<ll> weight(N);
-    vector<int> value(N);
-    int total_value = 0;
-    for (int i = 0; i < N; ++i) {
-        cin >> weight[i] >> value[i];
-        total_value += value[i];
-    }
-    vector<ll> dp(total_value+1, 2e9);
-    dp[0] = 0;
-    for (int i = 0; i < N; ++i) {
-        for (int v = total_value; v >= value[i]; --v) {
-            dp[v] = min(dp[v], dp[v - value[i]] + weight[i]);
-        }
-    }
-    for (int v = total_value; v >= 0; --v) {
-        if (dp[v] <= W) {
-            cout << v << '\n';
-            return;
-        }
-    }
-}
+typedef pair<int, int> pii;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    Main();
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, maxw, sumv;
+	cin >> n >> maxw;
+	sumv = 0;
+	vector<int> itemw(n), itemv(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> itemw[i] >> itemv[i];
+		sumv += itemv[i];
+	}
+	vector<int> dp(sumv+1, maxw+1);
+	dp[0] = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int v = sumv-itemv[i]; v >= 0; --v) {
+			dp[v+itemv[i]] = min(dp[v+itemv[i]], dp[v] + itemw[i]);
+		}
+	}
+	for (int v = sumv; v >= 0; --v) {
+		if (dp[v] != -1 && dp[v] <= maxw) {
+			cout << v << '\n';
+			return 0;
+		}
+	}
 }
