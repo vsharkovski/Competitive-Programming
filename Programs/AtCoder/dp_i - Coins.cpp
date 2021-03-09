21b2a0a40e140bc6f7f6673149cf3763430fdc26
vlatkosh <vsarkovskii@gmail.com>
2021-03-09 20:08:37 +0100

    Update dp_i - Coins.cpp

--- Programs/AtCoder/dp_i - Coins.cpp
+++ Programs/AtCoder/dp_i - Coins.cpp
@@ -1,49 +1,29 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-//using namespace __gnu_pbds;
 typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-template <class Key, class Compare = less<Key>>
-using Tree = __gnu_pbds::tree<Key, __gnu_pbds::null_type, Compare, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+typedef pair<int, int> pii;
 
-
-
-
-
-
-void Main() {
-  int n;
-  cin >> n;
-  static double p[3000];
-  for (int i = 0; i < n; ++i) {
-    cin >> p[i];
-  }
-  // dp[i][h] = probability of having h heads after throwing coins [0..i-1]
-  static double dp[3000][3000];
-  dp[0][0] = 1.0;
-  for (int i = 0; i < n; ++i) {
-    for (int h = 0; h <= i; ++h) {
-      dp[i+1][h+1] += p[i]       * dp[i][h];
-      dp[i+1][h]   += (1 - p[i]) * dp[i][h];
-    }
-  }
-  double ans = 0;
-  for (int h = n / 2 + (n % 2); h <= n; ++h) {
-    ans += dp[n][h];
-  }
-  cout << setprecision(15) << ans << '\n';
-}
 
 int main() {
-  ios::sync_with_stdio(false);
-//  cin.tie(nullptr);
-  #ifdef _DEBUG
-//  freopen("in.txt", "r", stdin);
-//  freopen("out.txt", "w", stdout);
-  #endif
-  Main();
-  return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<double> cur(n+1), nxt(n+1);
+	cur[0] = 1;
+	for (int i = 0; i < n; ++i) {
+		double p;
+		cin >> p;
+		for (int h = 0; h < n; ++h) {
+			nxt[h+1] += cur[h] * p;
+			nxt[h] += cur[h] * (1 - p);
+		}
+		swap(cur, nxt);
+		nxt.assign(n+1, 0);
+	}
+	double ans = 0;
+	for (int h = (n+1)/2; h <= n; ++h) {
+		ans += cur[h];
+	}
+	cout << setprecision(15) << ans << '\n';
 }
