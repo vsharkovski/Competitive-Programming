25e3e08b8ce87ddc66dbacd54a1379c38fa8a426
vlatkosh <vsarkovskii@gmail.com>
2021-03-09 19:35:15 +0100

    Update dp_g - Longest Path.cpp

--- Programs/AtCoder/dp_g - Longest Path.cpp
+++ Programs/AtCoder/dp_g - Longest Path.cpp
@@ -1,56 +1,39 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-//using namespace __gnu_pbds;
 typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-template <class Key, class Compare = less<Key>>
-using Tree = __gnu_pbds::tree<Key, __gnu_pbds::null_type, Compare, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+typedef pair<int, int> pii;
 
+const int N = 100100;
 
-
-
-int N, M;
-vector<int> G[100000];
-int dp[100000];
+int n, m, ans;
+vector<int> adj[N];
+int dp[N];
 
 int dfs(int u) {
-  int& res = dp[u];
-  if (res != -1) {
-    return res;
-  }
-  res = 0;
-  for (int v : G[u]) {
-    res = max(res, 1 + dfs(v));
-  }
-  return res;
-}
-
-void Main() {
-  cin >> N >> M;
-  for (int i = 0; i < M; ++i) {
-    int x, y;
-    cin >> x >> y;
-    --x, --y;
-    G[x].push_back(y);
-  }
-  memset(dp, -1, sizeof(dp));
-  int ans = 0;
-  for (int i = 0; i < N; ++i) {
-    ans = max(ans, dfs(i));
-  }
-  cout << ans << '\n';
+	if (dp[u] != -1) return dp[u];
+	dp[u] = 0;
+	for (int v : adj[u]) {
+		dp[u] = max(dp[u], 1+dfs(v));
+	}
+	ans = max(ans, dp[u]);
+	return dp[u];
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-//  cin.tie(nullptr);
-  #ifdef _DEBUG
-//  freopen("in.txt", "r", stdin);
-//  freopen("out.txt", "w", stdout);
-  #endif
-  Main();
-  return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < m; ++i) {
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+	}
+	for (int i = 1; i <= n; ++i) {
+		dp[i] = -1;
+	}
+	ans = 0;
+	for (int i = 1; i <= n; ++i) {
+		dfs(i);
+	}
+	cout << ans << '\n';
 }
