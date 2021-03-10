82a813e72f74a62c4e5a32aedd590373b2344c15
vlatkosh <vsarkovskii@gmail.com>
2021-03-10 21:16:23 +0100

    Update dp_m - Candies.cpp

--- Programs/AtCoder/dp_m - Candies.cpp
+++ Programs/AtCoder/dp_m - Candies.cpp
@@ -1,66 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-
-
-
+typedef pair<int, int> pii;
 
 const int mod = 1e9 + 7;
 
-inline void add_self(int& x, int y) {
-    x += y;
-    if (x >= mod) x -= mod;
-    if (x < 0) x += mod;
-}
-
-inline void add_range(vector<int>& v, int x, int l, int r) {
-    add_self(v[l], x);
-    add_self(v[r+1], -x);
-}
-
-void Main() {
-    int N, K;
-    cin >> N >> K;
-    vector<int> a(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> a[i];
-    }
-    vector<int> curr(K+2, 0);
-    curr[K] = 1;
-    for (int i = 0; i < N; ++i) {
-        // make difference array for easy range updates
-        vector<int> diff(K+2, 0);
-        add_range(diff, curr[0], 0, 0);
-        for (int rem = 1; rem <= K; ++rem) {
-            add_range(diff, curr[rem], rem - min(rem, a[i]), rem);
-        }
-        // make real array from difference array
-        // this would be the "next" array, and i would set curr = next later,
-        // but i can just set it directly
-        curr[0] = diff[0];
-        for (int rem = 1; rem <= K; ++rem) {
-            curr[rem] = curr[rem-1];
-            add_self(curr[rem], diff[rem]);
-        }
-    }
-    // O(N*K^2)
-//    for (int i = 0; i < N; ++i) {
-//        vector<int> next(K+1, 0);
-//        add_self(next[0], curr[0]);
-//        for (int rem = 1; rem <= K; ++rem) {
-//            for (int give = 0; give <= min(rem, a[i]); ++give) {
-//                add_self(next[rem - give], curr[rem]);
-//            }
-//        }
-//        curr = next;
-//    }
-    cout << curr[0] << '\n';
+inline int add(int x, int y) {
+	x += y;
+	if (x >= mod) x -= mod;
+	if (x < 0) x += mod;
+	return x;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    Main();
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m; // m candies total
+	cin >> n >> m;
+	vector<int> pre(m+1, 0), cur(m+1);
+	pre[0] = 1;
+	for (int i = 0; i < n; ++i) {
+		int a;
+		cin >> a;
+		for (int k = 0; k <= m; ++k) {
+			if (k) pre[k] = add(pre[k], pre[k-1]);
+			cur[k] = pre[k];
+			if (k-a-1 >= 0) cur[k] = add(cur[k], -pre[k-a-1]);
+			// dp[i][k] = sum(dp[i-1][j]) as j=k-a[i] to k
+			// i.e cur[k] = sum(pre[j]) as j=k-a[i] to k
+			// we can make pre a prefix sum array and use that
+		}
+		swap(pre, cur);
+	}
+	cout << pre[m] << '\n';
 }
