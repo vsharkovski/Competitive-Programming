911c70d1316b987bf9021d2a2477c2213048f653
vlatkosh <vsarkovskii@gmail.com>
2021-03-10 20:56:12 +0100

    Update dp_l - Deque.cpp

--- Programs/AtCoder/dp_l - Deque.cpp
+++ Programs/AtCoder/dp_l - Deque.cpp
@@ -1,58 +1,32 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-//using namespace __gnu_pbds;
 typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-template <class Key, class Compare = less<Key>>
-using Tree = __gnu_pbds::tree<Key, __gnu_pbds::null_type, Compare, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+typedef pair<int, int> pii;
 
-
-
-int N;
-ll a[3000];
-ll dp[3003][3003][2];
-
-ll dfs(int i, int j, int p) {
-  ll& res = dp[i][j][p];
-  if (res != -1) {
-    return res;
-  } else {
-    if (p == 0) { // taro
-      if (i == j) {
-        return res = a[i];
-      } else {
-        return res = max(a[i] + dfs(i+1, j, 1), a[j] + dfs(i, j-1, 1));
-      }
-    } else { // jiro
-      if (i == j) {
-        return res = -a[i];
-      } else {
-        return res = min(-a[i] + dfs(i+1, j, 0), -a[j] + dfs(i, j-1, 0));
-      }
-    }
-  }
-}
-
-void Main() {
-  cin >> N;
-  for (int i = 0; i < N; ++i) {
-    cin >> a[i];
-  }
-  memset(dp, -1, sizeof(dp));
-  ll ans = dfs(0, N-1, 0);
-  cout << ans << '\n';
-}
 
 int main() {
-  ios::sync_with_stdio(false);
-//  cin.tie(nullptr);
-  #ifdef _DEBUG
-//  freopen("in.txt", "r", stdin);
-//  freopen("out.txt", "w", stdout);
-  #endif
-  Main();
-  return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	const int N = 3003;
+	int n;
+	cin >> n;
+	ll a[N];
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	static ll dp[N][N][2];
+	for (int i = 0; i < n; ++i) {
+		dp[i][i][0] = a[i];
+		dp[i][i][1] = -a[i];
+	}
+	for (int d = 1; d < n; ++d) {
+		for (int i = 0; i+d < n; ++i) {
+			int j = i+d;
+			// taro (maximize)
+			dp[i][j][0] = max(a[i] + dp[i+1][j][1], a[j] + dp[i][j-1][1]);
+			// jiro (minimize)
+			dp[i][j][1] = min(-a[i] + dp[i+1][j][0], -a[j] + dp[i][j-1][0]);
+		}
+	}
+	cout << dp[0][n-1][0] << '\n';
 }
