67e2932259569fd02ede38cf23b16d1534aa6359
vlatkosh <vsarkovskii@gmail.com>
2021-03-11 13:17:58 +0100

    Update dp_u - Grouping.cpp

--- Programs/AtCoder/dp_u - Grouping.cpp
+++ Programs/AtCoder/dp_u - Grouping.cpp
@@ -1,83 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
+typedef pair<int, int> pii;
 
-
-
-
-const ll unset = -1e18;
-const ll inf = 1e15;
-
-
-int N;
-ll a[16][16];
-ll total[(1<<16) + 5];
-ll dp[(1<<16) + 5];
-
-ll dfs(int msk) {
-    ll& res = dp[msk];
-    if (res == unset) {
-        if (msk + 1 == (1 << N)) {
-            res = 0;
-        } else {
-            res = -inf;
-            vector<int> available;
-//            cout << "msk=" << bitset<3>(msk) << " ; available:[";
-            for (int i = 0; i < N; ++i) {
-                if (!(msk & (1 << i))) {
-                    available.push_back(i);
-//                    cout << i << ' ';
-                }
-            }
-//            cout << "]\n";
-            int cnt = available.size();
-            for (int opt = 1; opt < (1 << cnt); ++opt) {
-                int msk1 = 0;
-                for (int i = 0; i < cnt; ++i) {
-                    if (opt & (1 << i)) {
-                        msk1 |= (1 << available[i]);
-                    }
-                }
-                ll score = total[msk1] + dfs(msk | msk1);
-                res = max(res, score);
-            }
-        }
-//        cout << "dp[" << bitset<3>(msk) << "] = " << res << "\n";
-    }
-    return res;
-}
-
-
-void Main() {
-    cin >> N;
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            cin >> a[i][j];
-        }
-    }
-    for (int msk = 0; msk < (1 << N); ++msk) {
-        dp[msk] = unset;
-        ll score = 0;
-        for (int i = 0; i < N; ++i) {
-            if (!(msk & (1 << i))) continue;
-            for (int j = i+1; j < N; ++j) {
-                if (!(msk & (1 << j))) continue;
-                score += a[i][j];
-            }
-        }
-        total[msk] = score;
-    }
-    ll ans = dfs(0);
-    cout << ans << '\n';
-}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    #ifndef _DEBUG
-    #endif
-    Main();
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	const int N = 16;
+	int n;
+	cin >> n;
+	int a[N][N];
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			cin >> a[i][j];
+		}
+	}
+	ll score[1<<N];
+	for (int m = 0; m < (1<<n); ++m) {
+		score[m] = 0;
+		for (int i = 0; i < n; ++i) {
+			if (!(m & (1 << i))) continue;
+			for (int j = i+1; j < n; ++j) {
+				if (!(m & (1 << j))) continue;
+				score[m] += a[i][j];
+			}
+		}
+	}
+	ll dp[1<<N];
+	dp[0] = 0;
+	for (int m = 1; m < (1<<n); ++m) {
+		dp[m] = 0; // when all are separate
+		for (int s = m; s > 0; s = (s-1) & m) {
+			dp[m] = max(dp[m], score[s] + dp[m & ~s]);
+		}
+	}
+	cout << dp[(1<<n)-1] << '\n';
 }
