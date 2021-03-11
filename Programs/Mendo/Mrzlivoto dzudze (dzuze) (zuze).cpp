145b335e557c8cb4484113f989a13485452bf421
vlatkosh <vsarkovskii@gmail.com>
2021-03-11 09:43:15 +0100

    Create Mrzlivoto dzudze (dzuze) (zuze).cpp

---
+++ Programs/Mendo/Mrzlivoto dzudze (dzuze) (zuze).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << n/b - n/a << '\n';
+}
