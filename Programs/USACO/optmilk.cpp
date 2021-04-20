41794423c11a9527b3d09b7f75110f9c003042df
vlatkosh <vsarkovskii@gmail.com>
2021-04-20 11:21:31 +0200

    Update optmilk.cpp

--- Programs/USACO/optmilk.cpp
+++ Programs/USACO/optmilk.cpp
@@ -1,72 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef vector<int> vi;
 
+using ll = long long;
 
+const int N = 40010;
+const int M = (1<<17) + 10;
 
-
+int n, d;
+int milk[N];
 
 struct Node {
-    ll yy, yn, ny, nn;
-    Node() {
-        yy = yn = ny = nn = 0;
-    }
-};
+	ll a00, a01, a10, a11;
+	inline void pull(Node &l, Node &r) {
+		a00 = max(l.a01 + r.a00, l.a00 + r.a10);
+		a01 = max(l.a01 + r.a01, l.a00 + r.a11);
+		a10 = max(l.a11 + r.a00, l.a10 + r.a10);
+		a11 = max(l.a11 + r.a01, l.a10 + r.a11);
+	}
+} tree[M];
 
-int n;
-Node tree[150000];
-
-void merge(Node& nd, Node& l, Node& r) {
-    nd.yy = max(l.yy + r.ny, l.yn + r.yy);
-    nd.yn = max(l.yy + r.nn, l.yn + r.yn);
-    nd.ny = max(l.ny + r.ny, l.nn + r.yy);
-    nd.nn = max(l.ny + r.nn, l.nn + r.yn);
+void build(int v = 1, int tl = 0, int tr = n-1) {
+	if (tl == tr) {
+		tree[v].a11 = milk[tl];
+		tree[v].a00 = tree[v].a01 = tree[v].a10 = 0;
+		return;
+	}
+	int tm = (tl + tr) / 2;
+	build(2*v, tl, tm);
+	build(2*v+1, tm+1, tr);
+	tree[v].pull(tree[2*v], tree[2*v+1]);
 }
 
-void build() {
-    for (int k = n-1; k >= 1; --k) {
-        merge(tree[k], tree[2*k], tree[2*k+1]);
-    }
-}
-
-void update(int k, int x) {
-    k += n;
-    tree[k].yy = x;
-    tree[k].yn = tree[k].ny = tree[k].nn = 0;
-    for (k /= 2; k >= 1; k /= 2) {
-        merge(tree[k], tree[2*k], tree[2*k+1]);
-    }
-}
-
-void Main() {
-    int m, d;
-    cin >> m >> d;
-    n = 1;
-    while (n < m) n *= 2;
-    for (int i = 0; i < m; ++i) {
-        cin >> tree[i + n].yy;
-    }
-    build();
-    ll answer = 0;
-    while (d--) {
-        int i, x;
-        cin >> i >> x;
-        --i;
-        update(i, x);
-        answer += tree[1].yy;
-    }
-    cout << answer << '\n';
+void update(int pos, int val, int v = 1, int tl = 0, int tr = n-1) {
+	if (tl == tr) {
+		tree[v].a11 = val;
+		return;
+	}
+	int tm = (tl + tr) / 2;
+	if (pos <= tm) {
+		update(pos, val, 2*v, tl, tm);
+	} else {
+		update(pos, val, 2*v+1, tm+1, tr);
+	}
+	tree[v].pull(tree[2*v], tree[2*v+1]);
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    #ifndef _DEBUG
-        freopen("optmilk.in", "r", stdin);
-        freopen("optmilk.out", "w", stdout);
-    #endif
-    Main();
-    return 0;
+	cin.tie(0)->sync_with_stdio(0);
+	#ifndef _DEBUG
+	freopen("optmilk.in", "r", stdin);
+	freopen("optmilk.out", "w", stdout);
+	#endif // _DEBUG
+	cin >> n >> d;
+	for (int i = 0; i < n; ++i) {
+		cin >> milk[i];
+	}
+	build();
+	ll ans = 0;
+	for (int i = 0; i < d; ++i) {
+		int pos, val;
+		cin >> pos >> val;
+		--pos;
+		update(pos, val);
+		ans += tree[1].a11;
+	}
+	cout << ans << '\n';
 }
