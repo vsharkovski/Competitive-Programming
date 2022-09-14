5bfe39023381aadbf47e535b2556a0b1dd6fe438
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-09-14 20:26:27 +0400

    Update 1585. Check If String Is Transformable With Substring Sort Operations.cpp

--- Programs/Leetcode/1585. Check If String Is Transformable With Substring Sort Operations.cpp
+++ Programs/Leetcode/1585. Check If String Is Transformable With Substring Sort Operations.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    const static int inf = 1e9;
     const static int A = 10;
     
+    // pos[d] = [all positions of digit d in string s]
+    // i.e. pos[d][i] = position of i-th occurence of digit d
     vector<int> pos[A];
+    // pos_idx[d] = how many times we have seen d so far
     int pos_idx[A];
     
     bool isTransformable(string s, string t) {
+        // initialize arrays
         for (int i = 0; i < A; ++i) {
             pos[i].clear();
             pos_idx[i] = 0;
         }
+        // fill pos array
         int n = s.size();    
         for (int i = 0; i < n; ++i) {
             int d = s[i] - '0';
             pos[d].push_back(i);
         }
+        // 
         for (int i = 0; i < n; ++i) {
             int d = t[i] - '0';
             if (pos_idx[d] == (int)pos[d].size()) {
